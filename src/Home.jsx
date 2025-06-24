a819ba9bd543041980cd6720d3547c782172d675
Ashik M <ashikmd1845@gmail.com>
2025-06-24 11:32:05 +0530

    tasks updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,11 +10,36 @@ const Home = () => {
       <h1 className="main-title">Mitbots Tasks</h1>
 
       <div className="section-row">
+  <h1 className="section-heading">JCREA Replicate</h1>
+  <button
+    className="see-button"
+    onClick={() =>
+      window.open("https://ashik-frontend-mitbots.netlify.app/", "_blank")
+    }
+  >
+    See
+  </button>
+</div>
+
+
+      <div className="section-row">
         <h1 className="section-heading">Multiple Cards Overlapping</h1>
         <button className="see-button" onClick={() => navigate("/cards")}>
           See
         </button>
       </div>
+
+      <div className="section-row">
+  <h1 className="section-heading">Quetor</h1>
+  <button
+    className="see-button"
+    onClick={() =>
+      window.open("https://quetor.com/", "_blank")
+    }
+  >
+    See
+  </button>
+</div>
 
       <div className="section-row">
         <h1 className="section-heading">What we offer replicate</h1>
@@ -24,11 +49,23 @@ const Home = () => {
       </div>
 
       <div className="section-row">
-        <h1 className="section-heading">Object moving</h1>
+        <h1 className="section-heading">3D Object three.js</h1>
         <button className="see-button" onClick={() => navigate("/object")}>
           See
         </button>
       </div>
+<div className="section-row">
+  <h1 className="section-heading">Portfolio</h1>
+  <button
+    className="see-button"
+    onClick={() =>
+      window.open("https://mitbots-portfolio.netlify.app/", "_blank")
+    }
+  >
+    See
+  </button>
+</div>
+      
     </div>
   );
 };
